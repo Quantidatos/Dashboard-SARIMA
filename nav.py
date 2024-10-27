f3a610b29c17dfb44bf60a5d6a70e4c63c65cc0a
Quantidatos <lecr.company.eirl@gmail.com>
2024-10-27 11:03:13 -0500

    assets

---
+++ nav.py
@@ -0,0 +1,19 @@
+from dash import html
+import dash_bootstrap_components as dbc
+import dash
+
+_nav = dbc.Container([
+	dbc.Row([
+        dbc.Col([
+            html.Div([
+                html.I(className="fa fa-line-chart")],
+		    className='logo')
+        ], width = 4),
+        dbc.Col([html.H1(['Modelo SARIMA'], className='app-brand')], width = 8)
+	]),
+	dbc.Row([
+        dbc.Nav(
+	        [dbc.NavLink(page["name"], active='exact', href=page["path"]) for page in dash.page_registry.values()],
+	        vertical=True, pills=True, class_name='my-nav')
+    ])
+])
